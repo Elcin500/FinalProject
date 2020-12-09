f46ae7ef2fca94caa7eda365cf394062a6850cda
Elcin500 <elcinha@code.edu.az>
2020-12-09 20:10:18 +0400

    multi lang

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,75 @@ $(document).ready(function () {
 
 //Header functions
 function header() {
+    //Header functions
+    let liLang = $('#lang');
+
+    let liLangSpan = $('#lang > span');
+
+    let liLangImg=$('.lang-img-main');
+
+    let hidden = $(`#lang-div`);
+
+    $(liLang).hover(function () {
+    
+        // console.log(hidden);
+        $(hidden).removeClass('none')    
+    }, function () {
+        // out
+        $(hidden).addClass('none');
+    });
+
+    
+    let langs = $(`#lang-div a `);
+
+
+    $.each(langs, function (index, element) { 
+         $(element).click(function (e) { 
+             e.preventDefault();
+             
+
+             let a=e.currentTarget;
+
+             span = e.currentTarget.getElementsByTagName('span');
+             img = e.currentTarget.getElementsByTagName('img');
+             imgsrc=$(img).attr('src');
+
+             $(liLangSpan).text($(span).text());
+
+             $(liLangImg).attr('src', imgsrc);
+             
+         });
+    });
+
+
+
+  $('#dropdownMenu2').click(function (e) { 
+      e.preventDefault();
+      if($('.hidden-dropdown').hasClass('dropdown-display')){
+        $('.hidden-dropdown').removeClass('dropdown-display');
+      }
+      else{
+        $('.hidden-dropdown').addClass('dropdown-display');
+      }
+  });
+
+  $.each($('.dropdown-menu-items'), function (index, insideMenuButtons) { 
+    
+     $(insideMenuButtons).find('button').click(function (e) { 
+         e.preventDefault();
+        //  console.log(e.currentTarget);
+        let hiddenMenu= $(insideMenuButtons).find('div');
+        if($(hiddenMenu).hasClass('dropdown-display')){
+            $(hiddenMenu).removeClass('dropdown-display');
+          }
+          else{
+            $(hiddenMenu).addClass('dropdown-display');
+          }
+         
+     });
+
+  });
+
 }
 
 // Footer
